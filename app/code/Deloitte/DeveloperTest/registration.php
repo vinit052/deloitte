53c6d8f58d5abb3a97c8cc0f896a6aa52ab3dbb2
Chirag Patel <chirag.patel@dotsquares.com>
2020-10-12 19:20:07 +0530

    add module

---
+++ app/code/Deloitte/DeveloperTest/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @category  Deloitte
+ * @package   Deloitte_DeveloperTest
+ * @author    Deloitte Team <info@deloitte.com>
+ * @copyright 2020 Deloitte 
+ * @link      https://www2.deloitte.com/
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Deloitte_DeveloperTest', __DIR__);
---
+++ app/etc/env.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'e4b339223e49d316106ec247fa5f668f'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento23',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 30 Sep 2020 11:30:54 +0000'
+    ]
+];
